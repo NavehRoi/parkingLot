3167117ef08c2f1d8cac6aa33119952366588992
NirNussbaum08 <nirnu@wix.com>
2024-05-20 14:09:29 +0300

    Final bash version

--- initParkingLot.sh
+++ initParkingLot.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install -y git curl jq
+sudo yum update -y
+sudo yum install git -y
 
-# Install Node.js and npm
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo yum install -y nodejs
 sudo npm install -g pm2
 
-# Clone the repository
 git clone https://github.com/NavehRoi/parkingLot.git
 cd parkingLot
 
-# Install application dependencies
 npm install
 
-# Retrieve MongoDB URI from AWS Systems Manager Parameter Store
 MONGODB_URI=$(aws ssm get-parameter --name "MONGODB_URI" --with-decryption --query "Parameter.Value" --output text)
 
-# Create .env file and add environment variables
 cat <<EOF > .env
 MONGODB_URI=$MONGODB_URI
 EOF
