6c339a59e09147a131008a96ec52e6c21f9a879c
Nir Nussbaum <140694045+NirNussbaum08@users.noreply.github.com>
2024-05-27 14:07:04 +0300

    Update initParkingLot.sh

--- initParkingLot.sh
+++ initParkingLot.sh
@@ -2,18 +2,11 @@
 
 sudo yum update -y
 sudo yum install git -y
-
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
 sudo npm install -g pm2
-
 git clone https://github.com/NavehRoi/parkingLot.git
 cd parkingLot
-
 npm install
-
 node server.js
-
 echo "Deployment completed successfully. Application is running on port 3000."
-
-
